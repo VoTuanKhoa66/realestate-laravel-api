70ff155e646eea446303000f4b89442edf292f14
ACER\khoav <khoa93076@donga.edu.vn>
2025-10-06 10:40:12 +0700

    Init Laravel backend and Property API

---
+++ app/Http/Controllers/Api/PropertyController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Property;
+use Illuminate\Http\Request;
+
+class PropertyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        return Property::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request->validate([
+            'title' => 'required',
+            'price' => 'required|numeric',
+            'area' => 'required|integer',
+            'address' => 'required',
+            'description' => 'nullable',
+        ]);
+        return Property::create($data);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
